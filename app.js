36c5819829a925e508cf6453196d86e3ff4bc865
이명한 <myunghan@imyeonghans-MacBook-Air.local>
2021-03-20 01:30:35 +0900

    4

--- app.js
+++ app.js
@@ -1,32 +1,33 @@
+// express, morgan
 import express from "express";
 import morgan from "morgan";
-import helmet from "helmet";
+// middleware
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
-import { userRouter } from "./router";
+import helmet from "helmet";
+import { localsMiddleware } from "./middlewares";
+//router
+import routes from "./routes";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
 
 const app = express();
-    
-const handleHome = (req, res) => {
-    console.log('home');
-    res.send("welcome to my home!!");
-}
 
-const handleProfile = (req, res) => {
-    console.log('profile');
-    res.send("welcom to my profile!!");
-}
+// view engine
+app.set("view engine", "pug");    
 
 // middleware 
+app.use(helmet());
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(helmet());
 app.use(morgan("dev"));
+app.use(localsMiddleware);
 
-//route
-app.get ("/", handleHome);
-app.get("/profile", handleProfile);
-app.use("/user", userRouter);
+//routes
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
